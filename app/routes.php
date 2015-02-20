d765bedd4a7857f4b671d845435ef0254f22df67
Andrew Clinton <ddrew.clinton@gmail.com>
2015-02-20 01:34:31 -0500

    added new routes

--- app/routes.php
+++ app/routes.php
@@ -1,25 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
- */
-
-Route::get('/', function () {
-	return View::make('hello');
-});
-
 // route to show the login form
-Route::get('login', array('uses' => 'HomeController@showLogin'));
+Route::get('login', array('uses' => 'SessionsController@showLogin'));
 
 // route to process the form
-Route::post('login', array('uses' => 'HomeController@doLogin'));
+Route::post('login', array('uses' => 'SessionsController@doLogin'));
 
 // route to process logout
-Route::get('logout', array('uses' => 'HomeController@doLogout'));
+Route::get('logout', array('uses' => 'SessionsController@doLogout'));
+
+// route to show reg form
+Route::get('sign-up', array('uses' => 'RegistrationController@showRegistration'));
+
+// route to process the reg form
+Route::post('sign-up', array('uses' => 'RegistrationController@doRegistration'));
+
+Route::group(array("before" => "auth"), function () {
+	Route::get('/', array('uses' => 'HomeController@showWelcome'));
+});
