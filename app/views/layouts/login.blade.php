06035a3f22214db5ccb285cad629ddaba70aca78
Andrew Clinton <ddrew.clinton@gmail.com>
2015-02-20 01:35:23 -0500

    added two new layouts

---
+++ app/views/layouts/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+    </head>
+    <body>
+
+        <div class="container-fluid">
+            @yield('content')
+        </div><!-- /.container -->
+
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
