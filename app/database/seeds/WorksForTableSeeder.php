7b2a8a1d3424c067006453ac011bf99835677f52
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-13 21:26:31 -0400

    fixed duplicate classname issue

--- app/database/seeds/WorksForTableSeeder.php
+++ app/database/seeds/WorksForTableSeeder.php
@@ -3,7 +3,7 @@
 // Composer: "fzaninotto/faker": "v1.3.0"
 use Faker\Factory as Faker;
 
-class WrittenByTableSeeder extends Seeder {
+class WorksForTableSeeder extends Seeder {
 
 	public function run()
 	{
