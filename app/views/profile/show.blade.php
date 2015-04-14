a85b5e4ba901609b9d3d431b4bc52050b27d025d
Andrew Clinton <ddrew.clinton@gmail.com>
2015-04-13 21:26:55 -0400

    add username to profile view

--- app/views/profile/show.blade.php
+++ app/views/profile/show.blade.php
@@ -4,6 +4,7 @@
 	<div class="row">
 		{{link_to_route('profiles.edit', 'Edit', array($user->id))}}
 		<div class="col-s-12">{{$user->name}}</div>
+		<div class="col-s-12">{{$user->username}}</div>
 		<div class="col-s-12">{{$user->email}}</div>
 		<div class="col-s-12">{{$user->address}}</div>
 	</div>
